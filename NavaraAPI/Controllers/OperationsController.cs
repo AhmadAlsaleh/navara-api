617f0713d124340a53bb2896d6ac5cc64c311f05
mohammed <mohammed@DESKTOP-KJH34TH>
2018-11-29 15:08:16 +0200

    Operation Order

--- NavaraAPI/Controllers/OperationsController.cs
+++ NavaraAPI/Controllers/OperationsController.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using NavaraAPI.ViewModels;
 using Newtonsoft.Json;
 using SmartlifeLtd.Data.Tables.Navara;
+using SmartLifeLtd.Classes;
 using SmartLifeLtd.Classes.Attribute;
 using SmartLifeLtd.Data.AspUsers;
 using SmartLifeLtd.Data.DataContexts;
@@ -48,7 +49,8 @@ namespace NavaraAPI.Controllers
                         );
                 if (Courses != null)
                 {
-                    return Json(JsonConvert.SerializeObject(Courses));
+           
+                    return Json(Courses);
                 }
                 else
                 {
@@ -62,6 +64,40 @@ namespace NavaraAPI.Controllers
             }
     
         }
+        [HttpGet]
+        public async Task<ActionResult> GetEvents()
+        {
+            try
+            {
+                var Events = _Context.Set<Event>()?.Include(s => s.Organiztaion)?.Where(s => s.IsActive == true).Select(a => new
+                {
+                    a.Title,
+                    a.Description,
+                    a.StartDate,
+                    OrganiztaionName = a.Organiztaion.Name,
+                    ContactName = a.Organiztaion.ContactName,
+                    a.IsActive,
+                    a.SessionsNumber
+                }
+                        );
+                if (Events != null)
+                {
+         
+                    return Json(Events);
+                }
+                else
+                {
+                    return BadRequest("Empty");
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+
+            }
+
+        }
+
         [HttpGet]
         public async Task<ActionResult> GetOrganiztaion()
         {
@@ -79,7 +115,7 @@ namespace NavaraAPI.Controllers
                         );
                 if (Organiztaions != null)
                 {
-                    return Json(JsonConvert.SerializeObject(Organiztaions));
+                    return Json( Organiztaions );
                 }
                 else
                 {
@@ -93,80 +129,57 @@ namespace NavaraAPI.Controllers
             }
 
         }
-        //[AuthorizeToken]
-        //[HttpPost]
-        //public async Task<IActionResult> NewOrderdItem([FromBody]ItemNewModel model)
-        //{
-            //try
-            //{
-            //    if (model == null) return BadRequest("Error while recieving data");
-            //    var userID = HttpContext.User.Identity.Name;
-            //    if (userID == null) return StatusCode(StatusCodes.Status401Unauthorized);
-            //    ApplicationUser user = await _context.Set<ApplicationUser>().SingleOrDefaultAsync(x => x.UserName == userID);
-            //    if (user == null) return BadRequest("Token is not related to any Account");
-            //    Account account = _context.Set<Account>().Include(x => x.Cart)
-            //        .ThenInclude(x => x.CartItems).FirstOrDefault(x => x.ID == user.AccountID);
-            //    if (user == null || account == null) return null;
-            //    ItemCategory cat = _context.Set<ItemCategory>().FirstOrDefault(x => x.IsUsed == true);
-            //    if (cat == null)
-            //        return StatusCode(StatusCodes.Status503ServiceUnavailable);
+        [AuthorizeToken]
+        [HttpPost]
+        public async Task<IActionResult> NewOrderedItem([FromBody]OrderedItemViewModel model)
+        {
+            try
+            {
+                if (model == null) return BadRequest("Error while recieving data");
+                var userID = HttpContext.User.Identity.Name;
+                if (userID == null) return StatusCode(StatusCodes.Status401Unauthorized);
+                ApplicationUser user = await _Context.Set<ApplicationUser>().SingleOrDefaultAsync(x => x.UserName == userID);
+                if (user == null) return BadRequest("Token is not related to any Account");
+                Account account = _Context.Set<Account>().Include(x => x.Cart)
+                    .ThenInclude(x => x.CartItems).FirstOrDefault(x => x.ID == user.AccountID);
+                if (user == null || account == null) return null;
 
-            //    Item item = new Item()
-            //    {
-            //        CashBack = 0,
-            //        CreationDate = DateTime.Now,
-            //        ItemCategoryID = cat.ID,
-            //        IsEnable = true,
+                OrderdItem Ordereditem = new OrderdItem()
+                {
+                    CreationDate = DateTime.Now,
+                    NeededDate = model.NeededDate,
+                    Name = model.Name,
+                    Description = model.Description,
+                    AccountID = account?.ID,
+                };
+                #region Save Other images
+                if (model.Images != null && model.Images.Any(x => x.Length > 0))
+                {
+                    foreach (var file in model.Images.Where(x => x.Length > 0))
+                    {
+                        var itemImage = new OrderdItemImage()
+                        {
+                            OrderdItem = Ordereditem
+                        };
+                        itemImage.ImagePath = ImageOperations.SaveItemImage(file, itemImage);
+                        if (!string.IsNullOrWhiteSpace(itemImage.ImagePath))
+                        {
+                            Ordereditem.OrderdItemImages.Add(itemImage);
+                        }
+                        else itemImage.OrderdItem = null;
+                    }
+                }
+                #endregion
 
-            //        Price = model.Price,
-            //        AccountID = account.ID,
-            //        Name = model.Name,
-            //        Quantity = model.Quantity,
-            //        Description = model.Description,
-            //        Owner = model.Owner,
-            //        Mobile = model.Mobile,
-            //        Location = model.Location,
-            //        ShortDescription = model.Description.Substring(0, Math.Min(25, model.Description.Length))
-            //    };
-            //    #region Save Main Image
-            //    if (model.Thumbnail != null && model.Thumbnail.Length > 0)
-            //    {
-            //        item.ThumbnailImagePath = ImageOperations.SaveItemImage(model.Thumbnail, item);
-            //        var itemImage = new ItemImage() { Item = item };
-            //        itemImage.ImagePath = ImageOperations.SaveItemImage(model.Thumbnail, itemImage);
-            //        if (!string.IsNullOrWhiteSpace(itemImage.ImagePath)) { item.ItemImages.Add(itemImage); }
-            //        else itemImage.Item = null;
-            //    }
-            //    #endregion
-
-            //    #region Save Other images
-            //    if (model.Images != null && model.Images.Any(x => x.Length > 0))
-            //    {
-            //        foreach (var file in model.Images.Where(x => x.Length > 0))
-            //        {
-            //            var itemImage = new ItemImage()
-            //            {
-            //                Item = item
-            //            };
-            //            itemImage.ImagePath = ImageOperations.SaveItemImage(file, itemImage);
-            //            if (!string.IsNullOrWhiteSpace(itemImage.ImagePath))
-            //            {
-            //                item.ItemImages.Add(itemImage);
-            //            }
-            //            else itemImage.Item = null;
-            //        }
-            //    }
-            //    #endregion
-
-            //    var data = _context.Set<Item>().Add(item);
-            //    _context.SaveChanges();
-            //    return Ok(item.ID);
-            //}
-            //catch (Exception ex)
-            //{
-            //    return BadRequest(ex.Message);
-            //}
-      //  }
+                var data = _Context.Set<OrderdItem>().Add(Ordereditem);
+                _Context.SaveChanges();
+                return Ok(Ordereditem.ID);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
         public ActionResult Chat()
         {
             return View("~/views/Index.cshtml");
---
+++ NavaraAPI/ViewModels/OrderedItemViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NavaraAPI.ViewModels
+{
+    public class OrderedItemViewModel
+    {
+        public Guid ID { set; get; }
+        public string Name { get; set; }
+
+        public DateTime? NeededDate { get; set; }
+        public string Description { get; set; }
+        public List<string> Images { get; set; }
+
+    }
+  
+}
